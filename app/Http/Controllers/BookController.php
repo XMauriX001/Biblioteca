41fcc8ad0b943be13e793a1ffde974635909d42e
XMauriX001 <mauriciorecinos021176@gmail.com>
2026-03-04 19:15:02 -0600

    Update BookController.php
    Endpoints faltantes

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -14,14 +14,44 @@ class BookController extends Controller
     {
         $this->authorize('viewAny', Book::class);
         $books = Book::when($request->has('title'), function ($query) use ($request) {
-            $query->where('title', 'like', '%'.$request->input('title').'%');
+            $query->where('title', 'like', '%' . $request->input('title') . '%');
         })->when($request->has('isbn'), function ($query) use ($request) {
-            $query->where('ISBN', 'like', '%'.$request->input('isbn').'%');
+            $query->where('ISBN', 'like', '%' . $request->input('isbn') . '%');
         })->when($request->has('is_available'), function ($query) use ($request) {
             $query->where('is_available', $request->boolean('is_available'));
         })
             ->paginate();
 
         return response()->json(BookResource::collection($books));
+    }
+
+    public function show(Book $book)
+    {
+        $this->authorize('view', $book);
+
+        return response()->json(BookResource::make($book));
+    }
+
+    public function store(Request $request, Book $book)
+    {
+        $this->authorize('create', Book::class);
+
+        $validated = $request->validate([
+            'title' => 'sometimes|string|max:255',
+            'description' => 'nullable|string',
+            'ISBN' => 'sometimes|string|unique:books,ISBN,' . $book->id,
+            'total_copies' => 'sometimes|integer|min:1',
+        ]);
+
+        $book->update($validated);
+
+        return response()->json(BookResource::make($book));
+    }
+
+    public function destroy(Book $book) 
+    {
+        $this->authorize('delete', $book);
+        $book->delete();
+        return response()->json(['message' => 'Book deleted successfully'], 200);
     }
 }
